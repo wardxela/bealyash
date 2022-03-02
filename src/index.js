3fcb867011e93ac06f59093021a05d5d8898fe09
wardxela <axelwardweb@gmail.com>
2022-03-02 18:43:11 +0300

    add: first request

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import { createServer } from 'https';
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
+const HOST = '0.0.0.0';
 
 const server = createServer((req, res) => {
+  console.log(req);
+  res.writeHead(200);
   res.end('hello');
 });
 
-server.listen(PORT);
+server.listen(PORT, HOST);
