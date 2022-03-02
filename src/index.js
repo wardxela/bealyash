adfdc3b665817f1c902193f3bb6872e0db5fffab
wardxela <axelwardweb@gmail.com>
2022-03-02 18:26:33 +0300

    fix: port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { createServer } from 'https';
 
+const PORT = process.env.PORT || 8080;
+
 const server = createServer((req, res) => {
   res.end('hello');
 });
 
-server.listen(8000);
+server.listen(PORT);
