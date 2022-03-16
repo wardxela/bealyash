3c37cf8e0d27272e8cb6c61b185d57a1f505d49e
wardxela <axelwardweb@gmail.com>
2022-03-16 16:15:23 +0300

    fix: sticker access

--- src/bot/commands/drink.js
+++ src/bot/commands/drink.js
@@ -1,7 +1,10 @@
 import BotResponse from '../utilities/BotResponse.js';
+import { anyElement } from '../utilities/random.js';
+
+const STICKERS = [67104, 51669, 16];
 
 export default async function drink(requestMessage, vkRequest) {
-    return new BotResponse({
-        message: '10604'
-    });
+  return new BotResponse({
+    sticker_id: anyElement(STICKERS),
+  });
 }
