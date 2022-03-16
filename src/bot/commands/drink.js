abe2e45a9e8a8046557c12384f4a930866e7c24b
Alexander Ward <63157919+wardxela@users.noreply.github.com>
2022-03-16 09:28:42 +0300

    fix 2

--- src/bot/commands/drink.js
+++ src/bot/commands/drink.js
@@ -2,6 +2,6 @@ import BotResponse from '../utilities/BotResponse.js';
 
 export default async function drink(requestMessage, vkRequest) {
     return new BotResponse({
-        sticker_id: '10604'
+        sticker_id: 10604
     });
 }
