8a7a1ff9e552f384dd43134a223d1a187b7bffcb
Alexander Ward <63157919+wardxela@users.noreply.github.com>
2022-03-16 09:20:43 +0300

    fix

--- src/bot/commands/drink.js
+++ src/bot/commands/drink.js
@@ -1,4 +1,4 @@
-import BotResponse from '../utilities.js';
+import BotResponse from '../utilities/BotResponse.js';
 
 export default async function drink(requestMessage, vkRequest) {
     return new BotResponse({
