204196032349fd006f0d76a87383f2dd910452d3
wardxela <axelwardweb@gmail.com>
2022-07-31 20:40:40 +0300

    woops

--- src/core/settings.ts
+++ src/core/settings.ts
@@ -1,10 +1,8 @@
-import { BotSettings } from '.';
+import { BotSettings } from './interfaces';
 
 export const settings: BotSettings = {
   confirmationToken: process.env.CONFIRMATION_STRING!,
   secretKey: process.env.SECRET_KEY!,
-  vkApiAccessToken:
-    process.env.VK_API_ACCESS_TOKEN! ||
-    'vk1.a.fcT_KAjdEDfX3ELnjBOqTfhMjQESSLOYiEVd3mRNKK3wnjZaMBKoWSjIcpb6o4RZBeF0gZB2BC119022grMyqSG3uRXpc8a8Zbjjz7Q9fjrXsBooTIa8teBeyMTw6EQG23pXX9Of4lJvsuKLnzI6cE7Vo1DfD6SSs2hIy1kcob3ws4rmyZkEqXcbsgJ-ms6Z',
-  vkApiVersion: process.env.VK_API_VERSION! || '5.131',
+  vkApiAccessToken: process.env.VK_API_ACCESS_TOKEN!,
+  vkApiVersion: process.env.VK_API_VERSION!,
 };
