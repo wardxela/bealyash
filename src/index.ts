e1bbd599dbfcb61523c94ee4567f4d11a941ae23
wardxela <axelwardweb@gmail.com>
2022-08-03 13:49:45 +0300

    Make chant command case-insensitive

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,7 @@ import { test, decrease, increase, chan } from './commands';
 const bot = createBot(config);
 bot.listen(+process.env.PORT! || 80);
 
-bot.add(/asdf/, () => {
-  return { message: 'asdf' };
-});
-
 bot.add(/тест/, test);
 bot.add(/твій пісюн зменшився/, decrease);
 bot.add(/твій пісюн виріс/, increase);
-bot.add(/тян/, chan);
+bot.add(/тян/i, chan);
