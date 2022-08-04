6262b1f14e7885896238778c8e8fc72cf39570bd
wardxela <axelwardweb@gmail.com>
2022-08-04 10:26:37 +0300

    Refactoring

--- src/services/gachi/drawDick.ts
+++ src/services/gachi/drawDick.ts
@@ -14,26 +14,26 @@ const BALLS = `...▄█▓░░░░░░░░░▓█▄
 .▀▄▒▒▒▒▒▄▀▒▀▄▒▒▒▒▒▄▀
 `;
 
-const PATTERN = `....▐░░░░░░░░░░░▌`;
+const BODY_PATTERN = `....▐░░░░░░░░░░░▌`;
 
-const REDUCING_VALUE = 10;
+const SPLIT_LENGTH = 10;
 const TAIL = 60;
 
-function drawBodyPart(pattern: string, reducedLength: number) {
+function drawBody(length: number) {
   let result = '';
-  for (let i = 0; i <= reducedLength; i += REDUCING_VALUE) {
-    result = `${result}${pattern} - ${TAIL + i} см\n`;
+  for (let i = TAIL; i <= length; i += SPLIT_LENGTH) {
+    result = `${result}${BODY_PATTERN} - ${i} см\n`;
   }
   return result;
 }
 
 export function drawDick(length: number) {
-  const COMMENT = `Все верно, длина твоего аппарата ${length} см.\n\n`;
-  const normalizedLength = length - TAIL;
-  if (normalizedLength < 0) {
+  if (length - TAIL < 0) {
     return `Мальчик, ты еще не дорос, чтобы показывать свое достояние в чате`;
   }
-  const BODY = drawBodyPart(PATTERN, normalizedLength);
+
+  const COMMENT = `Все верно, длина твоего аппарата ${length} см.\n\n`;
+  const BODY = drawBody(length);
 
   return `${COMMENT}${HEAD}${BODY}${BALLS}`;
 }
