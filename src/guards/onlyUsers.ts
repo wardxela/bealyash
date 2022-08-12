0fc1810622f6ee25f0f1777869811dc06d2df4d9
wardxela <axelwardweb@gmail.com>
2022-08-12 12:48:13 +0300

    Add exception for onlyUsers filter

--- src/guards/onlyUsers.ts
+++ src/guards/onlyUsers.ts
@@ -1,5 +1,13 @@
 import { BotGuard } from '../core';
 
+const EXCEPTIONS = [-193557157];
+
 export const onlyUsers: BotGuard = event => {
+  const fromId = event.object.message.from_id;
+
+  if (EXCEPTIONS.includes(fromId)) {
+    return true;
+  }
+
   return event.object.message.from_id > 0;
 };
