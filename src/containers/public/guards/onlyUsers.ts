5e5f13def91fa4d5539a6428d0fded09fd0ee7be
wardxela <axelwardweb@gmail.com>
2022-08-13 12:19:38 +0300

    Optimization: call guards only when the pattern matches and remove exceptions from onlyUsers guard

--- src/containers/public/guards/onlyUsers.ts
+++ src/containers/public/guards/onlyUsers.ts
@@ -1,14 +1,5 @@
 import { BotGuard } from '../../../core';
-import { DICK_KRAFT_BOT_ID } from '../../../services/gachi';
-
-const EXCEPTIONS = [DICK_KRAFT_BOT_ID];
 
 export const onlyUsers: BotGuard = event => {
-  const fromId = event.object.message.from_id;
-
-  if (EXCEPTIONS.includes(fromId)) {
-    return true;
-  }
-
   return event.object.message.from_id > 0;
 };
--- src/core/events/newMessageHandler.ts
+++ src/core/events/newMessageHandler.ts
@@ -15,30 +15,29 @@ export async function newMessageHandler(
     for (const nestedContainer of container.containers) {
       await newMessageHandler(event, nestedContainer, reply, config);
     }
-    for (const [pattern, guard] of container.guards) {
-      if (!doMatch(pattern, event.object.message.text)) {
-        continue;
-      }
-      const success = await safePromise(guard(event), timeout);
-      if (!success) {
-        return;
-      }
-    }
     for (const [pattern, command] of container.commands) {
       const match = event.object.message.text.match(pattern);
       if (!match) {
         continue;
       }
+      for (const [pattern, guard] of container.guards) {
+        if (!doMatch(pattern, event.object.message.text)) {
+          continue;
+        }
+        const success = await safePromise(guard(event), timeout);
+        if (!success) {
+          return;
+        }
+      }
       const commandResponse = await safePromise(command(event, match), timeout);
       if (verifyCommandResponse(commandResponse)) {
-        await reply(commandResponse, event);
+        return reply(commandResponse, event);
       }
-      break;
     }
   } catch (e) {
     const badCommandResponse = uncaughtCommandErrorResponse
       ? uncaughtCommandErrorResponse
       : DEFAULT_UNCAUGHT_COMMAND_ERROR_RESPONSE;
-    await reply(badCommandResponse, event);
+    return reply(badCommandResponse, event);
   }
 }
