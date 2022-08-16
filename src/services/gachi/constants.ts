01f4aaa6b813055fc78bf58ed4b96403ddc2f947
wardxela <axelwardweb@gmail.com>
2022-08-16 17:51:57 +0300

    Add phrases

--- src/services/gachi/constants.ts
+++ src/services/gachi/constants.ts
@@ -2,12 +2,15 @@ export const POSITIVE_REACTIONS_ALBUM_ID = 285411529;
 export const NEGATIVE_REACTIONS_ALBUM_ID = 285405543;
 
 export const POSITIVE_REACTIONS = [
-  'Как неожиданно и приятно',
   'So fucking deep',
   "Without further interruption, let's celebrate and suck some dick",
   'That turns me on',
   'Boss of this gym',
   "That's amazing",
+  'Alright man',
+  'Ass we can',
+  'ATTEEEN TION',
+  'Big surprise',
 ];
 
 export const NEGATIVE_REACTIONS = [
@@ -17,6 +20,18 @@ export const NEGATIVE_REACTIONS = [
   'Do you like what you see?',
   'Come on college boy',
   'Ahh, like that?',
+  'Autsch oh that hurts',
+  'Auuuuuugh how ya like that huh',
+  "Let's beat your ass",
+  'Boy next door',
+  'Finished with you GET UP',
+  'Get your ass down from here now boy',
+  'OOH IT HURTS',
+  'How ya feeling',
+  'How ya like dat huh',
+  "Oh shit, I'm sorry",
+  'Swallow my cum',
+  'You give up',
 ];
 
 export const DICK_KRAFT_BOT_ID = -193557157;
