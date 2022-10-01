0f83db353c7336f1ffb7698002f2cc655e23918c
wardxela <axelwardweb@gmail.com>
2022-10-01 15:32:17 +0300

    feat: enhance getProfile command

--- src/containers/public/commands/statistics/getProfile.ts
+++ src/containers/public/commands/statistics/getProfile.ts
@@ -1,30 +1,55 @@
+import * as R from 'rambda';
 import { BotCommand } from '../../../../core';
-import { findOrCreateProfile } from '../../../../services/db';
+import { db, findOrCreateProfile } from '../../../../services/db';
 import {
   createVkMemberLink,
+  getConversationMembers,
   getGroups,
   getUsers,
 } from '../../../../services/vk';
 import { getTimeDiff, ruNumberToString, timeToString } from '../../../../utils';
+import {
+  calcProbability,
+  createCoefficientMap,
+} from '../../../../utils/probabilities';
 
 export const getProfile: BotCommand = async event => {
   const { from_id, peer_id } = event.object.message;
   const profilePromise = findOrCreateProfile(from_id, peer_id);
   const memberPromise = from_id > 0 ? getUsers(from_id) : getGroups(-from_id);
+  const membersPromise = getConversationMembers(peer_id);
+  const profilesPromise = db.profile.findMany({
+    where: {
+      chat: { id: peer_id },
+      boosterExpirationDate: { gt: new Date() },
+      booster: { category: { title: 'Gay' } },
+    },
+    select: {
+      userId: true,
+      booster: { select: { value: true } },
+    },
+  });
 
-  const [profile, member] = await Promise.all([profilePromise, memberPromise]);
+  const [profile, member, members, profiles] = await Promise.all([
+    profilePromise,
+    memberPromise,
+    membersPromise,
+    profilesPromise,
+  ]);
+  const ids = R.pluck('member_id', members.response.items);
+  const coefficientMap = createCoefficientMap(
+    profiles,
+    'userId',
+    'booster.value'
+  );
 
-  const name = createVkMemberLink(member.response[0]);
-  const xp = `Опыт - ${profile.xp} XP`;
-  const gay = `Пидор - ${ruNumberToString(profile.gayCounter)}`;
-  const roles = !profile.rolesOnProfile.length
-    ? 'Не имеешь ни одной роли'
-    : `Имеешь роли: ${profile.rolesOnProfile.reduce(
-        (acc, { role }) => `${!acc ? acc : `${acc}, `}${role.name}`,
-        ''
-      )}`;
+  const roles =
+    profile.rolesOnProfile.reduce(
+      (acc, { role }) => `${!acc ? acc : `${acc}, `}${role.name}`,
+      ''
+    ) || 'нету';
 
-  let booster = 'Не имеешь буста';
+  let booster = 'нету';
 
   if (
     profile.booster &&
@@ -35,9 +60,20 @@ export const getProfile: BotCommand = async event => {
     booster = `Буст - "${profile.booster.title}" (${expiresIn})`;
   }
 
-  const message = `${name}, твоя статистика:\n${xp}\n${gay}\n${roles}\n${booster}`;
+  const message = `&#128202; ${createVkMemberLink(
+    member.response[0]
+  )}, твой профиль:
+  &#11088; Опыт - ${profile.xp} XP
+  &#127987;&#65039;&#8205;&#127752; Пидор - ${ruNumberToString(
+    profile.gayCounter
+  )}
+  &#128101; Роли - ${roles}
+  &#9889; Буст - ${booster}
+  &#128175; Шанс опидорения - ${calcProbability(
+    coefficientMap,
+    ids,
+    profile.userId
+  )}%`;
 
-  return {
-    message,
-  };
+  return { message };
 };
